ba2735d2372f04abb16247080c54448817add9c1
fedos <fedrodj@gmail.com>
2025-09-30 11:27:34 +0300

    :wrench: add some personal keymaps for nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,23 @@ vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]])
 vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]])
 vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]])
 vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]])
+
+local map = vim.keymap.set
+
+map("n", "<leader>yf", function()
+  local s = vim.fn.expand("%:t")
+  vim.fn.setreg(vim.v.register, s)
+  vim.notify("Yanked filename: " .. s)
+end, { desc = "Yank Filename" })
+
+map("n", "<leader>yp", function()
+  local s = vim.fn.expand("%")
+  vim.fn.setreg(vim.v.register, s)
+  vim.notify("Yanked relative path: " .. s)
+end, { desc = "Yank Relative Path" })
+
+map("n", "<leader>yP", function()
+  local s = vim.fn.expand("%:p")
+  vim.fn.setreg(vim.v.register, s)
+  vim.notify("Yanked absolute path: " .. s)
+end, { desc = "Yank Absolute Path" })
