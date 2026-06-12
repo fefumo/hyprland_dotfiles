782c51aaa99346a243a25eca2575a508b53ce056
fedos <fedrodj@gmail.com>
2026-06-12 18:05:21 +0300

    :bookmark: hyprland switch to lua

---
+++ hypr/hyprland.lua
@@ -0,0 +1,197 @@
+---@diagnostic disable-next-line: undefined-global
+local hl = hl
+
+require("hyprland_zoom")
+require("animations")
+require("keybinds")
+
+-- This is an example Hyprland Lua config file.
+-- Refer to the wiki for more information.
+-- https://wiki.hypr.land/Configuring/Start/
+
+-- Please note not all available settings / options are set here.
+-- For a full list, see the wiki
+
+-- You can (and should!!) split this configuration into multiple files
+-- Create your files separately and then require them like this:
+-- require("myColors")
+
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "eDP-1",
+	mode = "2560x1600@60",
+	position = "0x0",
+	scale = "1.25",
+})
+
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto-right",
+	scale = "1",
+})
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+-- local terminal = "alacritty"
+
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+-- hl.on("hyprland.start", function ()
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
+
+hl.on("hyprland.start", function()
+	hl.exec_cmd("hyprctl setcursor nier_cursors 32")
+	hl.exec_cmd("nm-applet")
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("avizo-service")
+	hl.exec_cmd("wl-paste --watch cliphist store")
+	hl.exec_cmd("~/.config/hypr/random_wall.sh")
+	hl.exec_cmd("~/.config/waybar/gen_waybar_theme.sh")
+	hl.exec_cmd("firefox", { workspace = "1" })
+end)
+
+hl.on("hyprland.start", function()
+	hl.exec_cmd("alacritty -e sh -lc 'nvim; exec $SHELL'", { workspace = "2" })
+	hl.exec_cmd("alacritty -e sh -lc 'htop; exec $SHELL'", { workspace = "5" })
+end)
+
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_THEME", "nier_cursors")
+-- hl.env("XCURSOR_SIZE", "24")
+-- hl.env("HYPRCURSOR_SIZE", "24")
+
+-----------------------
+----- PERMISSIONS -----
+-----------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+-- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
+-- for security reasons
+
+-- hl.config({
+--   ecosystem = {
+--     enforce_permissions = true,
+--   },
+-- })
+
+-- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+-- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+-- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+	input = {
+		kb_layout = "us, ru",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "grp:alt_shift_toggle",
+		kb_rules = "",
+
+		follow_mouse = 1,
+
+		sensitivity = 0.3, -- -1.0 - 1.0, 0 means no modification.
+
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
+})
+
+hl.gesture({
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
+})
+
+-- Example per-device config
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
+hl.device({
+	name = "logitech-g304-1",
+	sensitivity = 0,
+})
+
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- PERSONAL
+hl.window_rule({
+	name = "opaque-firefox",
+	match = {
+		class = "firefox",
+	},
+	opaque = 1,
+})
+
+-- Example window rules that are useful
+
+local suppressMaximizeRule = hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+
+	suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
+
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
+
+	no_focus = true,
+})
+
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
+
+	move = "20 monitor_h-120",
+	float = true,
+})
