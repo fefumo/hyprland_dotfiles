0bf819f8ad34dec0e9209a346cdef9fa4a775561
fedos <fedrodj@gmail.com>
2026-07-23 20:44:15 +0300

    :sparkles: new keymaps for yanking filepaths

--- nvim/config/keymaps.lua
+++ nvim/config/keymaps.lua
@@ -4,3 +4,22 @@
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "terminal -> normal" })
 vim.keymap.set("n", "<Leader>qw", "<cmd>wqall<cr>", { desc = "write & quit all buffers" })
+
+vim.keymap.set("n", "<leader>yd", function()
+  local dir = vim.fn.expand("%:p:h")
+  vim.fn.setreg("+", dir)
+  vim.notify("Copied: " .. dir)
+end, { desc = "Copy absolute dirpath" })
+
+vim.keymap.set("n", "<leader>yf", function()
+  local dir = vim.api.nvim_buf_get_name(0)
+  vim.fn.setreg("+", dir)
+  vim.notify("Copied: " .. dir)
+end, { desc = "Copy absolute filepath" })
+
+vim.keymap.set("n", "<leader>yF", function()
+  local relativeDir = vim.fn.expand("%:p:.")
+  vim.fn.setreg("+", relativeDir)
+  vim.notify("Copied: " .. relativeDir)
+end, {desc = "Copy relative filepath"})
+
